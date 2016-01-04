f6b2902cc6ccfd12b1b980e71eb33cc3d9a711b2
Matthew Shapiro <me@mshapiro.net>
2016-01-04 16:22:43 -0600

    Removed ConfigureServices method

--- src/ConsoleAspNetExample/Infrastructure/WebHost.cs
+++ src/ConsoleAspNetExample/Infrastructure/WebHost.cs
@@ -39,6 +39,8 @@ namespace ConsoleAspNetExample.Infrastructure
                 .UseServer("Microsoft.AspNet.Server.Kestrel")
                 .UseServices(x =>
                 {
+                    x.AddMvc();
+
                     foreach (var additionalService in additionalServices)
                     {
                         x.Add(additionalService);
@@ -53,13 +55,6 @@ namespace ConsoleAspNetExample.Infrastructure
         public void Dispose()
         {
             _application?.Dispose();
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
